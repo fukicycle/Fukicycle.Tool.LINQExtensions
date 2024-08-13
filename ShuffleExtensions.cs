e1786a6a7cf3be438e898213cca45fc45710d285
fukicycle <lego.sato.4135@gmail.com>
2024-08-13 16:22:05 +0900

    Add shuffle extensions

---
+++ ShuffleExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Fukicycle.Tool.LINQExtensions;
+
+public static class ShuffleExtensions
+{
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> src)
+    {
+        return src.OrderBy(a => Guid.NewGuid());
+    }
+}
